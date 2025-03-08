59de80b67ebb3b685c06c74a921ae8d249472864
Shaelyn26 <shaelynb0405@gmail.com>
2025-03-08 15:07:02 +0000

    added functioning

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,41 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+  const [activeColor, setActiveColor] = useState("red");
+  const lightStyle = (color) => ({
+    backgroundColor: color,
+    boxShadow: activeColor === color ? `0 0 30px 10px ${color}` : "none",
+  });
+  return (
+    <div className="container d-flex">
+      <div className="main-box bg-dark align-items-center mx-auto">
+        <div
+          className="red light bg-danger circle mx-auto"
+          style={lightStyle("red")}
+          onClick={() => setActiveColor("red")}
+        >
+          {" "}
+        </div>
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+        <div
+          className="yellow light bg-warning circle mx-auto"
+          style={lightStyle("yellow")}
+          onClick={() => setActiveColor("yellow")}
+        >
+          {" "}
+        </div>
+
+        <div
+          className="green light bg-success circle mx-auto"
+          style={lightStyle("green")}
+          onClick={() => setActiveColor("green")}
+        >
+          {" "}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
